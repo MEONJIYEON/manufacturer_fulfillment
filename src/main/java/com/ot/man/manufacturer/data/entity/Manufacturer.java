9eec9252a321e14a26735a2f1fbf9da8cb21f885
Inchan kim <kiminchan12341@gmail.com>
2024-05-19 16:53:15 +0900

    modify entity
    modify entity

--- src/main/java/com/ot/man/manufacturer/data/entity/Manufacturer.java
+++ src/main/java/com/ot/man/manufacturer/data/entity/Manufacturer.java
@@ -1,6 +1,5 @@
 package com.ot.man.manufacturer.data.entity;
 
-
 import java.time.LocalDateTime;
 
 import javax.persistence.Column;
@@ -8,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
@@ -18,21 +18,35 @@ import lombok.Data;
 @Table(name= "M_O_Manufacturer")
 public class Manufacturer {
 	
+	//출고번호 
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "out_number_seq")
 	@SequenceGenerator(name = "out_number_seq", sequenceName = "OUT_NUMBER_SEQ", allocationSize = 1)
-	@Column(nullable = false, unique = true)
-	private long out_number; //출고번호 
+	private long out_number; 
 	
+	 // 상품명
 	@Column(nullable = false)
-	private String out_pname; // 상품명
+	private String out_pname;
 	
-	@Column(nullable = false)
-	private long out_stock; // 출고 개수
 	
+	// 출고 개수
 	@Column(nullable = false)
-	private LocalDateTime out_date = LocalDateTime.now(); // 출고 날짜 (현재 시간으로 초기화)
+	private long out_stock; 
 	
+	//출고 시작(출고 중) 날짜 (현재 시간으로 초기화)
+	private LocalDateTime out_start_at;
+	
+	//출고 완료 날짜
+	private LocalDateTime out_complete_at;
+	
+	// 출고 처리 내역 : T : 촐고 완료 , F: 출고 중 
 	@Column(nullable = false)
-	private boolean out_history; // 출고 처리 내역 
+	private boolean out_status; 
+	
+	@PrePersist
+    protected void onCreate() {
+		out_start_at = LocalDateTime.now();
+    }
+
+	
 }
